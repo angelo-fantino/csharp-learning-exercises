0c31d7f6b6128d3abd493f23128712f4b66639d7
Angelo One <fantino.angelo@gmail.com>
2026-08-22 12:00:03 +0200

    feat(fraudulent_order): Add nested iteration exercise.

---
+++ 01_cs_fundamentals/projects_nested_iteration_fraudulent_order/projects_nested_iteration_fraudulent_order/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace projects_nested_iteration_fraudulent_order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // arrays_foreach loop
+            string[] fraudulentOrderID = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+            Console.Write("The following orders start with the letter B: ");
+
+            foreach (string currentOrderID in fraudulentOrderID)
+            {
+                if (currentOrderID.StartsWith("B"))
+                {
+                    Console.Write($"{currentOrderID}, ");
+
+                }
+            }
+            ;
+
+            Console.Write("\n");
+        }
+    }
+}
